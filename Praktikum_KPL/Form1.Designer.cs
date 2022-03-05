5dca81d4d97806bd02177b2b578fb6eb193b6ad4
Abib-Basnuril <91238142+Abib-Basnuril@users.noreply.github.com>
2022-03-05 16:52:11 +0700

    Add files via upload
    1A 2A

---
+++ Praktikum_KPL/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Praktikum_KPL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.samadengan = new System.Windows.Forms.Button();
+            this.tambah = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(141, 43);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n1.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n1.Location = new System.Drawing.Point(92, 120);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(49, 44);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n2.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n2.Location = new System.Drawing.Point(138, 120);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(49, 44);
+            this.n2.TabIndex = 2;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n3.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n3.Location = new System.Drawing.Point(184, 120);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(49, 44);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n6.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n6.Location = new System.Drawing.Point(184, 161);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(49, 44);
+            this.n6.TabIndex = 4;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n5.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n5.Location = new System.Drawing.Point(138, 161);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(49, 44);
+            this.n5.TabIndex = 5;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n4.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n4.Location = new System.Drawing.Point(92, 161);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(49, 44);
+            this.n4.TabIndex = 6;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            this.n4.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // n9
+            // 
+            this.n9.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n9.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n9.Location = new System.Drawing.Point(184, 199);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(49, 44);
+            this.n9.TabIndex = 7;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n8
+            // 
+            this.n8.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n8.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n8.Location = new System.Drawing.Point(138, 199);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(49, 44);
+            this.n8.TabIndex = 8;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n7
+            // 
+            this.n7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n7.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n7.Location = new System.Drawing.Point(92, 199);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(49, 44);
+            this.n7.TabIndex = 9;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // n0
+            // 
+            this.n0.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.n0.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n0.Location = new System.Drawing.Point(184, 79);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(49, 44);
+            this.n0.TabIndex = 10;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            this.n0.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // samadengan
+            // 
+            this.samadengan.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.samadengan.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.samadengan.Location = new System.Drawing.Point(138, 79);
+            this.samadengan.Name = "samadengan";
+            this.samadengan.Size = new System.Drawing.Size(49, 44);
+            this.samadengan.TabIndex = 11;
+            this.samadengan.Text = "=";
+            this.samadengan.UseVisualStyleBackColor = false;
+            this.samadengan.Click += new System.EventHandler(this.samadengan_Click);
+            // 
+            // tambah
+            // 
+            this.tambah.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tambah.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tambah.Location = new System.Drawing.Point(92, 79);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(49, 44);
+            this.tambah.TabIndex = 12;
+            this.tambah.Text = "+";
+            this.tambah.UseVisualStyleBackColor = false;
+            this.tambah.Click += new System.EventHandler(this.tambah_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(337, 285);
+            this.Controls.Add(this.tambah);
+            this.Controls.Add(this.samadengan);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button n1;
+        private Button n2;
+        private Button n3;
+        private Button n6;
+        private Button n5;
+        private Button n4;
+        private Button n9;
+        private Button n8;
+        private Button n7;
+        private Button n0;
+        private Button samadengan;
+        private Button tambah;
+    }
+}
